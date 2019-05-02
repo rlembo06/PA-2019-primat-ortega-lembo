9b5624da8032ca203412cd6e970f5f5c21d4440a
Jeremy Primat <valk.jeremy@gmail.com>
2019-05-03 01:01:33 +0200

    Add new weapons plugins

--- plugins/src/main/java/weapons/Mamitraillette.java
+++ plugins/src/main/java/weapons/Mamitraillette.java
@@ -2,13 +2,49 @@ package weapons;
 
 import annotations.weapons.Powerful;
 import annotations.weapons.Weapon;
+import entities.Player;
+import entities.Players;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 
 @Weapon
 public class Mamitraillette {
 
+    private static final double speed = 80.0;
     public int damage = 25;
     public int maxAmmunition = 10;
+    private ArrayList<Bullet> bullets;
+
+    public void keyboardBullets(Scene scene, Player player){
+        scene.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.SPACE){
+                bullets.add(new Bullet(player.getLocation().getX(), player.getLocation().getY()));
+            }
+        });
+    }
+
+    public void drawBullet(GraphicsContext gc){
+        for (Bullet bullet : bullets) {
+            double x = bullet.getX();
+            double y = bullet.getY();
+
+            gc.setFill(Color.ORANGE);
+            gc.fillRect(x, y, 20, 10);
+        }
+    }
+
+    public void shotBullets(Player player, Players[] players){
+        for (Bullet bullet : bullets) {
+            bullet.setX(bullet.getX() + speed);
+            bullet.setY(bullet.getY());
+        }
+    }
 
     @Powerful
     public int shot(int bullets) {
